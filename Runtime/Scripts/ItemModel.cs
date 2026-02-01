a41d4afc908c38d5197fa5a0a396a1745ce869db
Baopg <pgbao13@gmail.com>
2026-02-02 00:53:17 +0700

    update ver 0.1.1

--- Runtime/Scripts/ItemModel.cs
+++ Runtime/Scripts/ItemModel.cs
@@ -1,11 +1,14 @@
-public class ItemModel
+namespace SP.Item
 {
-    public string Id;
-    public double Quantity;
-
-    public ItemModel(string id, double quantity)
+    public class ItemModel
     {
-        Id = id;
-        Quantity = quantity;
+        public string Id;
+        public double Quantity;
+
+        public ItemModel(string id, double quantity)
+        {
+            Id = id;
+            Quantity = quantity;
+        }
     }
 }
